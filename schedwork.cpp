86ad5524f1a2711788af53a46cab5d31d9a7e885
Vaneet Saini <vsaini@usc.edu>
2022-12-04 05:59:03 +0000

    submission

--- schedwork.cpp
+++ schedwork.cpp
@@ -19,6 +19,25 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool isValid(
+    unsigned int row, 
+    unsigned int col,
+    unsigned int ID,
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched
+);
+
+bool scheduleHelper(
+    unsigned int row,
+    unsigned int col,
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched
+);
+
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -35,9 +54,95 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
-
-
-
-
+    unsigned int row, col = 0;
+    unsigned int len = avail.size();
+    for (unsigned int i = 0; i < len; ++i)
+    {
+        vector<Worker_T> temp;
+        for (unsigned int j = 0; j < dailyNeed; ++j)
+        {
+            temp.push_back(INVALID_ID);
+        }
+        sched.push_back(temp);
+    }
+    return scheduleHelper(row, col, avail, dailyNeed, maxShifts, sched);
 }
+
+bool isValid(
+    unsigned int row, 
+    unsigned int col,
+    unsigned int ID,
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched
+)
+{
+    bool ret = true; //default 
+    if (avail[row][ID] == 0) return false;
+    else if (avail[row][ID] == 1)
+    {
+        unsigned int num = 0;
+        unsigned int len = sched.size();
+        for (unsigned int i = 0; i < len; ++i)
+        {
+            for (unsigned int j = 0; j < dailyNeed; ++j)
+            {
+                if (sched[i][j] == ID) num++;
+            }
+        }
+        if (maxShifts < num)
+        {
+            ret = false;
+        }
+        if (ret == true)
+        {
+            for (unsigned int i = 0; i < dailyNeed; ++i)
+            {
+                if (sched[row][i] == ID) ret = false;
+            }
+        }
+    }
+    return ret;
+}
+
+bool scheduleHelper(
+    unsigned int row,
+    unsigned int col,
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched
+)
+{
+    unsigned int len = sched.size();
+    if (len == row)
+    {
+        for (unsigned int i = 0; i < len; ++i)
+        {
+            for (unsigned int j = 0; j < sched[0].size(); ++j)
+            {
+                if (sched[i][j] == INVALID_ID) return false;
+            }
+        }
+        return true;
+    }
+    unsigned int size = avail[0].size();
+    for (unsigned int i = 0; i < size; ++i)
+    {
+        //segementation fault 
+       /*if (isValid(row, col, i, avail, dailyNeed, maxShifts, sched))
+        {
+            sched[row][col] = i;
+            if (dailyNeed-1 != col)
+            {
+                if (scheduleHelper(row, col+1, avail, dailyNeed, maxShifts, sched)) return true;
+                else sched[row][col] = INVALID_ID;
+            }
+        }*/
+    } 
+    return false;
+}
+
+
 
--- wordle.cpp
+++ wordle.cpp
@@ -8,10 +8,17 @@
 #include "dict-eng.h"
 using namespace std;
 
+void guessHelper(
+	std::set<std::string>& guessList,
+	unsigned int current_index,
+	unsigned int spaceAmt,
+	const std::string& in,
+	std::string& _floating,
+	std::string& current,
+	const std::set<std::string>& dict
+);
 
 // Add prototypes of helper functions here
-
-
 // Definition of primary wordle function
 std::set<std::string> wordle(
     const std::string& in,
@@ -19,7 +26,75 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+		
+    unsigned int current_index = 0; //index to iterate through in
+    std::set<std::string> guessList; //set of guesses
+		std::string _floating = floating;
+		std::string current = "";
+		unsigned int spaceAmt = 0;
+		unsigned int len = in.size();
+		for (unsigned int i = 0; i < len; ++i)
+		{
+			if (in[i] == '-') spaceAmt++;
+		}
+		std::cout << spaceAmt << endl;
+		guessHelper(guessList, current_index, spaceAmt, in, _floating, current, dict);
+		return guessList;
+    
+}
+// Define any helper functions here
+void guessHelper(
+	std::set<std::string>& guessList,
+	unsigned int current_index,
+	unsigned int spaceAmt,
+	const std::string& in,
+	std::string& _floating,
+	std::string& current,
+	const std::set<std::string>& dict
+)
+{
+	unsigned int len = in.size();
+	if (current.size() == len)
+	{
+		if (dict.find(current) == dict.end()) return;
+		else
+		{
+			guessList.insert(current);
+			return;
+		}
+	}
+	else
+	{
+		if (in[current_index] != '-')
+		{
+			current += in[current_index];
+			guessHelper(guessList, current_index+1, spaceAmt, in, _floating, current, dict);
+			current.pop_back();
+			return;
+		}
+		else
+		{
+			for (unsigned int i = 0; i < _floating.size(); ++i)
+			{
+				current += _floating[i];
+				std::string tmp = _floating;
+				_floating.erase(i, 1);
+				guessHelper(guessList, current_index+1, spaceAmt-1, in, _floating, current, dict);
+				_floating = tmp;
+				current.pop_back();
+			}
+			if (spaceAmt-1 >= _floating.size())
+			{
+				current += 'a';
+				for (int i = 0; i < 26; ++i)
+				{
+					guessHelper(guessList, current_index+1, spaceAmt-1, in, _floating, current, dict);
+					current[current_index]++;
+				}
+				current.pop_back();
+			}
+			return;
+		}
+	}
 }
 
-// Define any helper functions here
